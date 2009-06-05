c21e6369104a6e8077765966cbfdf23c80ea76d0
acidre <info@example.com>
2009-06-05 15:41:55 +0000

    Initial import noveniniupdate
    git-svn-id: http://svn.projects.ez.no/noveniniupdate@1 1af223e5-867a-40b0-8df5-d3ec6ca2138b

---
+++ settings/menu.ini.append.php
@@ -0,0 +1,24 @@
+<?php /* #?ini charset="utf-8"?
+
+[NavigationPart]
+Part[noveniniupdatenavigationpart]=Noven: advanced INI parameters
+
+[TopAdminMenu]
+Tabs[]=noveniniupdate
+
+[Topmenu_noveniniupdate]
+NavigationPartIdentifier=noveniniupdatenavigationpart
+Name=Noven : Paramètres avancés INI
+Tooltip=Noven : édition de paramètres avancés INI en fonction de l'environnement
+URL[]
+URL[default]=noveniniupdate/view
+Enabled[]
+Enabled[default]=true
+Enabled[browse]=false
+Enabled[edit]=false
+Shown[]
+Shown[default]=true
+Shown[navigation]=true
+Shown[browse]=false
+
+*/ ?>
---
+++ settings/noveniniupdate.ini.append.php
@@ -0,0 +1,11 @@
+<?php /* #?ini charset="utf-8"?
+
+[XmlSettings]
+XmlContent=extension/noveniniupdate/source/sample.xml
+XmlType[array]=Array
+XmlType[true/false]=True / False
+XmlType[enable/disable]=Enabled / Disabled
+XmlType[string]=String
+XmlType[numeric]=Numeric
+
+*/ ?>
